4f6a3c8eaa7edaf3411bdffedc6ee9e85309f3fc
Anton Andersson <62707484+anton87andersson@users.noreply.github.com>
2023-01-18 07:48:27 +0100

    Create pico_w_temp.py

---
+++ pico_w_temp.py
@@ -0,0 +1,81 @@
+import network
+import time
+from machine import Pin
+from umqtt.simple import MQTTClient
+from machine import ADC
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("SSID","PASSWORD")
+time.sleep(5)
+print(wlan.isconnected())
+print(wlan.ifconfig())
+
+adc = machine.ADC(4) 
+LED = machine.Pin("LED", machine.Pin.OUT)
+
+#mqtt config
+mqtt_server = '192.168.50.223'
+client_id = 'PicoW'
+user_t = USERNAME'
+password_t = 'PASSWORD'
+topic_pub = 'hello'
+
+last_message = 0
+message_interval = 5
+counter = 0
+
+# the following will set the seconds between 2 keep alive messages
+keep_alive=30
+
+#MQTT connect
+def mqtt_connect():
+    client = MQTTClient(client_id, mqtt_server, user=user_t, password=password_t, keepalive=60)
+    client.connect()
+    print('Connected to %s MQTT Broker'%(mqtt_server))
+    return client
+
+#reconnect & reset
+def reconnect():
+    print('Failed to connected to MQTT Broker. Reconnecting...')
+    time.sleep(5)
+    machine.reset()
+
+# This code is executed once a new message is published
+def new_message_callback(topic, msg):
+    topic , msg=topic.decode('ascii') , msg.decode('ascii')
+    print("Topic: "+topic+" | Message: "+msg)
+    if msg == "off":
+        LED.off()
+    if msg == "on":
+        LED.on()
+
+try:
+    client = mqtt_connect()
+    client.set_callback(new_message_callback)
+    client.subscribe(topic_pub.encode('utf-8'))
+
+except OSError as e:
+    reconnect()
+
+last_message=time.time()
+
+# Main loop
+while True:
+    try:
+        client.check_msg()
+        time.sleep(0.001)
+        if (time.time() - last_message) > keep_alive:
+            ADC_voltage = adc.read_u16() * (3.3 / (65535))
+            temperature_celcius = 27 - (ADC_voltage - 0.706)/0.001721
+            temp_fahrenheit=32+(1.8*temperature_celcius)
+            client.publish(topic_pub, str(round(temperature_celcius,1)))
+            last_message = time.time()
+              
+
+    except OSError as e:
+        print(e)
+        reconnect()
+        pass
+
+client.disconnect()
